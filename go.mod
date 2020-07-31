67bcf665365ac84ce39af53bfa6efd5d42e1c313
wamuir <17497234+wamuir@users.noreply.github.com>
2020-07-31 00:02:53 -0700

    testify; new tests

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/wamuir/go-xslt
 
 go 1.14
+
+require github.com/stretchr/testify v1.6.1
--- xslt_test.go
+++ xslt_test.go
@@ -1,40 +1,67 @@
 package xslt
 
-import "testing"
+import (
+	"testing"
 
-const (
-	xml = `<?xml version="1.0" encoding="utf-8" ?>
+	"github.com/stretchr/testify/assert"
+)
+
+var xml = []byte(
+	`<?xml version="1.0" encoding="utf-8" ?>
 <rodents>
 <rodent>gopher</rodent>
-</rodents>`
+</rodents>`,
+)
 
-	xsl = `<?xml version="1.0" encoding="utf-8"?>
+var xsl = []byte(
+	`<?xml version="1.0" encoding="utf-8"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
 <xsl:template match="/">
 <xsl:for-each select="rodents/rodent">
 <h1><xsl:value-of select="."/></h1>
 </xsl:for-each>
 </xsl:template>
-</xsl:stylesheet>
-`
-
-	exp = `<?xml version="1.0"?>
-<h1>gopher</h1>
-`
+</xsl:stylesheet>`,
 )
 
-func TestTransform(t *testing.T) {
+var exp = []byte(
+	`<?xml version="1.0"?>
+<h1>gopher</h1>
+`,
+)
 
-	html, err := Transform([]byte(xsl), []byte(xml))
-	if err != nil {
-		t.Error(err)
-	}
+func TestTransform_A(t *testing.T) {
 
-	if string(html) != exp {
-		t.Errorf(
-			"Transform was incorrect, got: %s, expect: %s.",
-			string(html),
-			exp,
-		)
-	}
+	out, err := Transform(xsl, xml)
+	assert.Nil(t, err)
+
+	assert.Equal(t, exp, out)
+}
+
+// Empty XML
+func TestTransform_B(t *testing.T) {
+
+	_, err := Transform(xsl, make([]byte, 0))
+	assert.EqualError(t, ErrXSLTFailure, err.Error())
+}
+
+// Empty XSL
+func TestTransform_C(t *testing.T) {
+
+	_, err := Transform(make([]byte, 0), xml)
+	assert.EqualError(t, ErrXSLTFailure, err.Error())
+}
+
+// Invalid XML
+func TestTransform_D(t *testing.T) {
+
+	_, err := Transform(xsl, xml[0:len(xml)-1])
+	assert.EqualError(t, ErrXSLTFailure, err.Error())
+}
+
+// Invalid XSL
+func TestTransform_E(t *testing.T) {
+
+	_, err := Transform(xsl[0:len(xsl)-1], xml)
+	assert.EqualError(t, ErrXSLTFailure, err.Error())
 }
