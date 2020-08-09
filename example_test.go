a28315a7040e5e2026d18468aecf4401de2b7f15
wamuir <17497234+wamuir@users.noreply.github.com>
2020-08-09 08:01:36 -0700

    reformat example output for godoc

--- example_test.go
+++ example_test.go
@@ -6,65 +6,69 @@ import (
 	"github.com/wamuir/go-xslt"
 )
 
-var doc = []byte(
-	`<?xml version="1.0" ?>
-	 <persons>
-	   <person username="JS1">
-	     <name>John</name>
-	     <family-name>Smith</family-name>
-	   </person>
-	   <person username="MI1">
-	     <name>Morka</name>
-	     <family-name>Ismincius</family-name>
-	   </person>
-	 </persons>`,
-)
-
-var style = []byte(
-	`<?xml version="1.0" encoding="UTF-8"?>
-	 <xsl:stylesheet
-	   version="1.0"
-	   xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
-	   xmlns="http://www.w3.org/1999/xhtml">
-	   <xsl:output method="xml" indent="yes" encoding="UTF-8"/>
-	   <xsl:template match="/persons">
-	     <html>
-	       <head>
-	         <title>Testing XML Example</title>
-	       </head>
-	       <body>
-	         <h1>Persons</h1>
-	         <ul>
-		   <xsl:apply-templates select="person">
-		     <xsl:sort select="family-name" />
-		   </xsl:apply-templates>
-		 </ul>
-	       </body>
-	     </html>
-	   </xsl:template>
-	   <xsl:template match="person">
-	     <li>
-	       <xsl:value-of select="family-name"/><xsl:text>, </xsl:text><xsl:value-of select="name"/>
-	     </li>
-	   </xsl:template>
-	 </xsl:stylesheet>`,
-)
-
 func Example() {
+	// doc is the document to be transformed
+	var doc = []byte(
+		`<?xml version="1.0" ?>
+		 <persons>
+		   <person username="JS1">
+		     <name>John</name>
+		     <family-name>Smith</family-name>
+		   </person>
+		   <person username="MI1">
+		     <name>Morka</name>
+		     <family-name>Ismincius</family-name>
+		   </person>
+		 </persons>`,
+	)
 
+	// style is the XSL stylesheet to be used for transformation
+	var style = []byte(
+		`<?xml version="1.0" encoding="UTF-8"?>
+		 <xsl:stylesheet
+		   version="1.0"
+		   xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+		   xmlns="http://www.w3.org/1999/xhtml">
+		   <xsl:output method="xml" indent="yes" encoding="UTF-8"/>
+		   <xsl:template match="/persons">
+		     <html>
+		       <head>
+			 <title>Testing XML Example</title>
+		       </head>
+		       <body>
+			 <h1>Persons</h1>
+			 <ul>
+			   <xsl:apply-templates select="person">
+			     <xsl:sort select="family-name" />
+			   </xsl:apply-templates>
+			 </ul>
+		       </body>
+		     </html>
+		   </xsl:template>
+		   <xsl:template match="person">
+		     <li>
+		       <xsl:value-of select="family-name"/><xsl:text>, </xsl:text><xsl:value-of select="name"/>
+		     </li>
+		   </xsl:template>
+		 </xsl:stylesheet>`,
+	)
+
+	// create a new stylesheet from style
 	xs, err := xslt.NewStylesheet(style)
 	if err != nil {
 		panic(err)
 	}
 	defer xs.Close()
 
+	// transform the document using the style
 	res, err := xs.Transform(doc)
 	if err != nil {
 		panic(err)
 	}
 
+	// print the result of the transformation
 	fmt.Println(string(res))
-	// output:
+	// Output:
 	// <?xml version="1.0" encoding="UTF-8"?>
 	// <html xmlns="http://www.w3.org/1999/xhtml">
 	//   <head>
