f5db11dae85bd91985168c9c7be4a6a6fb9de936
William Muir <wamuir@gmail.com>
2021-12-14 15:52:23 -0600

    use table-driven tests

--- xslt_test.go
+++ xslt_test.go
@@ -1,120 +1,140 @@
-package xslt
+package xslt_test
 
 import (
 	"bytes"
-	_ "embed"
+	"io/ioutil"
 	"testing"
+
+	"github.com/wamuir/go-xslt"
 )
 
-//go:embed testdata/document.xml
-var document []byte
-
-//go:embed testdata/style1.xsl
-var style1 []byte
-
-//go:embed testdata/style2.xsl
-var style2 []byte
-
-//go:embed testdata/result1.xml
-var result1 []byte
-
-//go:embed testdata/result2.xhtml
-var result2 []byte
-
 func TestNewStylesheet(t *testing.T) {
+	tests := []struct {
+		name    string
+		xslFile string
+	}{
+		{"style1", "testdata/style1.xsl"},
+		{"style2", "testdata/style2.xsl"},
+	}
 
-	xs1, err := NewStylesheet(style1)
-	if err != nil {
-		t.Errorf("err = %v; want %v", err, nil)
-	}
-	if xs1 == nil {
-		t.Errorf("xs1 = %v, want non-nil", xs1)
-	}
-	defer xs1.Close()
+	for _, c := range tests {
+		t.Run(c.name, func(t *testing.T) {
+			xsl, _ := ioutil.ReadFile(c.xslFile)
 
-	xs2, err := NewStylesheet(style2)
-	if err != nil {
-		t.Errorf("err = %v; want %v", err, nil)
-	}
-	if xs2 == nil {
-		t.Errorf("xs2 = %v, want non-nil", xs2)
-	}
-	defer xs2.Close()
+			xs, err := xslt.NewStylesheet(xsl)
+			if err != nil {
+				t.Errorf("got: %v, want: %v", err, nil)
+			}
 
-	xs3, err := NewStylesheet(nil)
-	if err != ErrXSLParseFailure {
-		t.Errorf("err = %v; want %v", err, ErrXSLParseFailure)
+			if xs == nil {
+				t.Errorf("got: %v, want: %v", xs, "non-nil")
+			}
+		})
 	}
-	if xs3 != nil {
-		t.Errorf("xs3 = %v, want %v", xs3, nil)
+
+	errorTests := []struct {
+		name   string
+		xslStr string
+		err    error
+	}{
+		{"emptyXSL", "", xslt.ErrXSLParseFailure},
+	}
+
+	for _, c := range errorTests {
+		t.Run(c.name, func(t *testing.T) {
+			xs, err := xslt.NewStylesheet([]byte(c.xslStr))
+			if xs != nil {
+				t.Errorf("got: %v, expected %v", xs, nil)
+			}
+
+			if err != c.err {
+				t.Errorf("got: %v, expected %v", err, c.err)
+			}
+		})
 	}
 }
 
 func TestStylesheetClose(t *testing.T) {
-
-	xs1, err := NewStylesheet(style1)
-	if err != nil {
-		t.Errorf("err = %v; want %v", err, nil)
+	tests := []struct {
+		name    string
+		xslFile string
+	}{
+		{"style1", "testdata/style1.xsl"},
+		{"style2", "testdata/style2.xsl"},
+		{"nil", ""},
 	}
-	if xs1 == nil {
-		t.Errorf("xs1 = %v, want non-nil", xs1)
-	}
-	xs1.Close()
 
-	xs2 := Stylesheet{}
-	xs2.Close()
+	for _, c := range tests {
+		t.Run(c.name, func(t *testing.T) {
+			xs := new(xslt.Stylesheet)
+			if len(c.xslFile) > 0 {
+				xsl, _ := ioutil.ReadFile(c.xslFile)
+				xs, _ = xslt.NewStylesheet(xsl)
+			}
+			func(xs *xslt.Stylesheet) {
+				defer func() {
+					if r := recover(); r != nil {
+						t.Errorf("unexpected panic: %v", r)
+					}
+				}()
+				xs.Close()
+			}(xs)
+		})
+	}
 }
 
 func TestStylesheetTransform(t *testing.T) {
-
-	xs1, err := NewStylesheet(style1)
-	if err != nil {
-		t.Errorf("err = %v; want %v", err, nil)
-	}
-	if xs1 == nil {
-		t.Errorf("xs1 = %v, want non-nil", xs1)
-	}
-	defer xs1.Close()
-
-	res1, err := xs1.Transform(document)
-	if err != nil {
-		t.Errorf("err = %v; want %v", err, nil)
-	}
-	if !bytes.Equal(result1, res1) {
-		t.FailNow()
+	tests := []struct {
+		name    string
+		xmlFile string
+		xslFile string
+		resFile string
+	}{
+		{"style1", "testdata/document.xml", "testdata/style1.xsl", "testdata/result1.xml"},
+		{"style2", "testdata/document.xml", "testdata/style2.xsl", "testdata/result2.xhtml"},
 	}
 
-	xs2, err := NewStylesheet(style2)
-	if err != nil {
-		t.Errorf("err = %v; want %v", err, nil)
-	}
-	if xs2 == nil {
-		t.Errorf("xs2 = %v, want non-nil", xs2)
-	}
-	defer xs2.Close()
+	for _, c := range tests {
+		t.Run(c.name, func(t *testing.T) {
+			xml, _ := ioutil.ReadFile(c.xmlFile)
+			xsl, _ := ioutil.ReadFile(c.xslFile)
 
-	res2, err := xs2.Transform(document)
-	if err != nil {
-		t.Errorf("err = %v; want %v", err, nil)
-	}
-	if !bytes.Equal(result2, res2) {
-		t.FailNow()
+			xs, _ := xslt.NewStylesheet(xsl)
+
+			got, err := xs.Transform(xml)
+			if err != nil {
+				t.Errorf("got: %v, want: %v", err, nil)
+			}
+
+			want, _ := ioutil.ReadFile(c.resFile)
+			if !bytes.Equal(got, want) {
+				t.Errorf("got: %s, want: %s", got, want)
+			}
+		})
 	}
 
-	xs3, err := NewStylesheet(style1)
-	if err != nil {
-		t.Errorf("err = %v; want %v", err, nil)
+	errorTests := []struct {
+		name    string
+		xmlStr  string
+		xslFile string
+		err     error
+	}{
+		{"emptyXML", "", "testdata/style2.xsl", xslt.ErrXSLTFailure},
 	}
-	if xs3 == nil {
-		t.Errorf("xs3 = %v, want non-nil", xs3)
-	}
-	defer xs3.Close()
 
-	res3, err := xs3.Transform(nil)
-	if err != ErrXSLTFailure {
-		t.Errorf("err = %v; want %v", err, ErrXSLTFailure)
-	}
-	if res3 != nil {
-		t.Errorf("xs3 = %v, want %v", res3, nil)
+	for _, c := range errorTests {
+		t.Run(c.name, func(t *testing.T) {
+			xsl, _ := ioutil.ReadFile(c.xslFile)
+
+			xs, _ := xslt.NewStylesheet(xsl)
+
+			got, err := xs.Transform([]byte(c.xmlStr))
+			if got != nil {
+				t.Errorf("got: %s, want: %v", got, nil)
+			}
+			if err != c.err {
+				t.Errorf("got: %v, want: %v", err, c.err)
+			}
+		})
 	}
 }
